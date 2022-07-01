14818561e6d335141ce59c0081d3daeb5ae32c06
SANDRO SEIVANE <1502503@formacao.iefp.pt>
2022-07-01 12:34:47 +0100

    Add project files.

---
+++ Agenda/Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿
+namespace Agenda
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textnome = new System.Windows.Forms.TextBox();
+            this.textMail = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.maskTelefone = new System.Windows.Forms.MaskedTextBox();
+            this.btnAdicionar = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.listNome = new System.Windows.Forms.ListBox();
+            this.listEmail = new System.Windows.Forms.ListBox();
+            this.listTelefone = new System.Windows.Forms.ListBox();
+            this.btnImprimir = new System.Windows.Forms.Button();
+            this.btnAlterar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnLimpar);
+            this.groupBox1.Controls.Add(this.btnAdicionar);
+            this.groupBox1.Controls.Add(this.maskTelefone);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textMail);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textnome);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(26, 23);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(195, 431);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Dados";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome";
+            // 
+            // textnome
+            // 
+            this.textnome.Location = new System.Drawing.Point(10, 42);
+            this.textnome.Name = "textnome";
+            this.textnome.Size = new System.Drawing.Size(179, 22);
+            this.textnome.TabIndex = 1;
+            // 
+            // textMail
+            // 
+            this.textMail.Location = new System.Drawing.Point(10, 94);
+            this.textMail.Name = "textMail";
+            this.textMail.Size = new System.Drawing.Size(179, 22);
+            this.textMail.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Email";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Telefone";
+            // 
+            // maskTelefone
+            // 
+            this.maskTelefone.Location = new System.Drawing.Point(10, 149);
+            this.maskTelefone.Mask = "(999) 000000000";
+            this.maskTelefone.Name = "maskTelefone";
+            this.maskTelefone.Size = new System.Drawing.Size(137, 22);
+            this.maskTelefone.TabIndex = 5;
+            // 
+            // btnAdicionar
+            // 
+            this.btnAdicionar.Location = new System.Drawing.Point(10, 216);
+            this.btnAdicionar.Name = "btnAdicionar";
+            this.btnAdicionar.Size = new System.Drawing.Size(157, 23);
+            this.btnAdicionar.TabIndex = 6;
+            this.btnAdicionar.Text = "Adicionar";
+            this.btnAdicionar.UseVisualStyleBackColor = true;
+            this.btnAdicionar.Click += new System.EventHandler(this.btnAdicionar_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(10, 261);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(157, 23);
+            this.btnLimpar.TabIndex = 7;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnSair);
+            this.groupBox2.Controls.Add(this.btnEliminar);
+            this.groupBox2.Controls.Add(this.btnAlterar);
+            this.groupBox2.Controls.Add(this.btnImprimir);
+            this.groupBox2.Controls.Add(this.listTelefone);
+            this.groupBox2.Controls.Add(this.listEmail);
+            this.groupBox2.Controls.Add(this.listNome);
+            this.groupBox2.Location = new System.Drawing.Point(249, 23);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(539, 431);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Listas";
+            // 
+            // listNome
+            // 
+            this.listNome.FormattingEnabled = true;
+            this.listNome.ItemHeight = 16;
+            this.listNome.Location = new System.Drawing.Point(0, 22);
+            this.listNome.Name = "listNome";
+            this.listNome.Size = new System.Drawing.Size(134, 356);
+            this.listNome.TabIndex = 0;
+            this.listNome.SelectedIndexChanged += new System.EventHandler(this.listNome_SelectedIndexChanged);
+            // 
+            // listEmail
+            // 
+            this.listEmail.FormattingEnabled = true;
+            this.listEmail.ItemHeight = 16;
+            this.listEmail.Location = new System.Drawing.Point(140, 21);
+            this.listEmail.Name = "listEmail";
+            this.listEmail.Size = new System.Drawing.Size(194, 356);
+            this.listEmail.TabIndex = 1;
+            this.listEmail.SelectedIndexChanged += new System.EventHandler(this.listEmail_SelectedIndexChanged);
+            // 
+            // listTelefone
+            // 
+            this.listTelefone.FormattingEnabled = true;
+            this.listTelefone.ItemHeight = 16;
+            this.listTelefone.Location = new System.Drawing.Point(340, 22);
+            this.listTelefone.Name = "listTelefone";
+            this.listTelefone.Size = new System.Drawing.Size(133, 356);
+            this.listTelefone.TabIndex = 2;
+            this.listTelefone.SelectedIndexChanged += new System.EventHandler(this.listTelefone_SelectedIndexChanged);
+            // 
+            // btnImprimir
+            // 
+            this.btnImprimir.Location = new System.Drawing.Point(155, 385);
+            this.btnImprimir.Name = "btnImprimir";
+            this.btnImprimir.Size = new System.Drawing.Size(75, 23);
+            this.btnImprimir.TabIndex = 3;
+            this.btnImprimir.Text = "Imprimir";
+            this.btnImprimir.UseVisualStyleBackColor = true;
+            this.btnImprimir.Click += new System.EventHandler(this.btnImprimir_Click);
+            // 
+            // btnAlterar
+            // 
+            this.btnAlterar.Location = new System.Drawing.Point(236, 385);
+            this.btnAlterar.Name = "btnAlterar";
+            this.btnAlterar.Size = new System.Drawing.Size(75, 23);
+            this.btnAlterar.TabIndex = 4;
+            this.btnAlterar.Text = "Alterar";
+            this.btnAlterar.UseVisualStyleBackColor = true;
+            this.btnAlterar.Click += new System.EventHandler(this.btnAlterar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(317, 385);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 5;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(398, 385);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(75, 23);
+            this.btnSair.TabIndex = 6;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // printDocument1
+            // 
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
+            // 
+            // printPreviewDialog1
+            // 
+            this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+            this.printPreviewDialog1.Document = this.printDocument1;
+            this.printPreviewDialog1.Enabled = true;
+            this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
+            this.printPreviewDialog1.Name = "printPreviewDialog1";
+            this.printPreviewDialog1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Agenda";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnAdicionar;
+        private System.Windows.Forms.MaskedTextBox maskTelefone;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textMail;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textnome;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnAlterar;
+        private System.Windows.Forms.Button btnImprimir;
+        private System.Windows.Forms.ListBox listTelefone;
+        private System.Windows.Forms.ListBox listEmail;
+        private System.Windows.Forms.ListBox listNome;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
+    }
+}
+
---
+++ Agenda/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Agenda
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void LimparCampos()
+        {
+            textnome.Clear();
+            textMail.Clear();
+            maskTelefone.Clear();
+            btnAdicionar.Text = "Adicionar";
+        }
+
+        private void btnAdicionar_Click(object sender, EventArgs e)
+        {
+
+            //Verrificar se o nome esta preenchido
+
+            if (textnome.Text.Length == 0 )
+            {
+                MessageBox.Show("O nome é um campo obrigatorio!", "Nome", MessageBoxButtons.OK,MessageBoxIcon.Warning);
+                textnome.Focus();
+                return;
+
+            }
+            //verificar email preenchido
+
+            if (textMail.Text.Length == 0)
+            {
+                MessageBox.Show("O Email é um campo obrigatorio!", "Nome", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                textMail.Focus();
+                return;
+
+            }
+
+            if (btnAdicionar.Text.Equals("Actualizar"))
+            {
+                listNome.Items[listNome.SelectedIndex] = textnome.Text;
+                listEmail.Items[listEmail.SelectedIndex] = textMail.Text;
+                listTelefone.Items[listTelefone.SelectedIndex] = maskTelefone.Text;
+                LimparCampos();
+                return;
+            }
+            // adicionar os dados as listas
+
+            listNome.Items.Add(textnome.Text);
+            listEmail.Items.Add(textMail.Text);
+            listTelefone.Items.Add(maskTelefone.Text);
+            btnAlterar.Enabled = true;
+            btnEliminar.Enabled = true;
+            btnImprimir.Enabled = true;
+            LimparCampos();
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            LimparCampos();
+        }
+
+
+
+        private void listNome_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //Sicronizar 
+            listEmail.SelectedIndex = listNome.SelectedIndex;
+            listTelefone.SelectedIndex = listNome.SelectedIndex;
+        }
+
+        private void listEmail_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //Sicronizar 
+            listNome.SelectedIndex = listEmail.SelectedIndex;
+            listTelefone.SelectedIndex = listEmail.SelectedIndex;
+        }
+
+        private void listTelefone_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //Sicronizar 
+            listNome.SelectedIndex = listTelefone.SelectedIndex;
+            listEmail.SelectedIndex = listTelefone.SelectedIndex;
+        }
+        private void VerificarLista()
+        {
+            if (listNome.Items.Count == 0)
+            {
+                btnAlterar.Enabled = false;
+                btnEliminar.Enabled = false;
+                btnImprimir.Enabled = false;
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            VerificarLista();
+        }
+
+        private void btnAlterar_Click(object sender, EventArgs e)
+        {
+            if ( listNome.SelectedIndex < 0 )
+            {
+                MessageBox.Show("Nao selecionou nenhum item", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            textnome.Text = listNome.SelectedItem.ToString();
+            textMail.Text = listEmail.SelectedItem.ToString();
+            maskTelefone.Text = listTelefone.SelectedItem.ToString();
+            btnAdicionar.Text = "Actualizar";
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (listNome.SelectedIndex < 0)
+            {
+                MessageBox.Show("Nao selecionou nenhum item", "Erro", 
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            DialogResult Confirmar = MessageBox.Show("Confirma eliminar?", "Eliminar Contacto",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+
+            if (Confirmar == DialogResult.Yes)
+            {
+
+                int posicao = listNome.SelectedIndex;
+                listNome.Items.RemoveAt(posicao);
+                listEmail.Items.RemoveAt(posicao);
+                listTelefone.Items.RemoveAt(posicao);
+                VerificarLista();
+            }
+
+            
+        }
+
+        private void btnImprimir_Click(object sender, EventArgs e)
+        {
+            printPreviewDialog1.ShowDialog();
+
+        }
+
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+
+            //imprimir texto personalizado
+
+            Font fonte1 = new Font("Verdana", 18);
+            Font fonte2 = new Font("Verdana", 10);
+
+            e.Graphics.DrawString("Lista de contactos", fonte1, new SolidBrush(Color.Blue), 100, 100);
+
+            e.Graphics.DrawString("Nome", fonte2, new SolidBrush(Color.Black), 100, 150);
+            e.Graphics.DrawString("Email", fonte2, new SolidBrush(Color.Black), 300, 150);
+            e.Graphics.DrawString("Telefone", fonte2, new SolidBrush(Color.Black), 500, 150);
+
+            int y = 180;
+            for(int linha = 0; linha < listNome.Items.Count;linha++)
+            {
+                e.Graphics.DrawString(listNome.Items[linha].ToString(), fonte2, new SolidBrush(Color.Black), 100, y);
+                e.Graphics.DrawString(listEmail.Items[linha].ToString(), fonte2, new SolidBrush(Color.Black), 300, y);
+                e.Graphics.DrawString(listTelefone.Items[linha].ToString(), fonte2, new SolidBrush(Color.Black), 500, y);
+
+                y += 15;
+            }
+
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
